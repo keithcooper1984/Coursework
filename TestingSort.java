189039372b52e8969c6430b2384e0fb23a35995e
Keith Cooper <72869410+keithcooper1984@users.noreply.github.com>
2021-02-14 22:35:57 +0000

    Update TestingSort.java

--- TestingSort.java
+++ TestingSort.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * @author Keith Cooper
- * @version 10/02/2021
+ * @version 14/02/2021
  * Examples of different sorting algorithms
+ * Insertion, bubble, merge, heap and quick sort methods
  */
 
 public class TestingSort {
@@ -11,18 +13,27 @@ public class TestingSort {
 	public static void main(String[] args) {
 
 
-		int[] arr = {31, 41, 59, 26, 41, 58};
-		System.out.println("Insertion Sort");
-		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
-		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(insertion(arr)) + "\n");
+		int[] arr = {3, 41, 52, 26, 38, 57, 9, 49};
+//		int[] arr = {98, 47, 0, 46, -219, 32, 98, -27};
+//		System.out.println("Insertion Sort");
+//		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
+//		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(insertion(arr)) + "\n");
+//		
+//		System.out.println("Bubble Sort");
+//		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
+//		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(bubble(arr)) + "\n");
 		
-		System.out.println("Bubble Sort");
-		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
-		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(bubble(arr)) + "\n");
+//		System.out.println("Merge Sort");
+//		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
+//		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(mergeSort(arr)) + "\n");
+//		
+//		System.out.println("Heap Sort");
+//		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
+//		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(heapSort(arr)) + "\n");
 		
-		System.out.println("Merge Sort");
+		System.out.println("Quick Sort");
 		System.out.println("INITIAL ARRAY:  " + Arrays.toString(arr));
-		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(mergeSort(arr)) + "\n");
+		System.out.println("FINAL SORTED ARRAY:  " + Arrays.toString(quickSort(arr, 0, arr.length-1)) + "\n");
 	}
 
 	public static int[] insertion(int[] numIn) {
@@ -101,5 +112,141 @@ public class TestingSort {
 		System.out.println("New Combined Array: " +Arrays.toString(combArr));
 		return combArr;
 	}
+	
+	//Heapsort Methods
+	public static int parent(int i)
+	{
+		return i/2;
+	}
+	
+	public static int left(int i)
+	{
+		return (2*i)+1;
+	}
 
+	public static int right(int i)
+	{
+		return (2*i)+2;
+	}
+	
+	public static int[] maxHeapify(int[] array, int i)
+	{
+		int l = left(i);
+		int r = right(i);
+		int largest;
+		System.out.println("Left = " + l + ", Right = " + r + ", i = " + i);
+		if (l <= array.length-1 && array[l] > array[i])
+		{
+			largest = l;
+		}
+		else 
+		{
+			largest = i;
+		}
+		if (r <= array.length-1 && array[r] > array[largest])
+		{
+			largest = r;
+		}
+		System.out.println("largest = " + largest);
+		System.out.println("Current Array: " + Arrays.toString(array));
+		if (largest != i)
+		{
+			int temp = array [i];
+			array[i] = array[largest];
+			array[largest] = temp;
+			maxHeapify(array,largest);
+		}
+		return array;
+	}
+	
+	public static int[] maxHeapify(int[] array, int i, int length)
+	{
+		int l = left(i);
+		int r = right(i);
+		int largest;
+		System.out.println("Left = " + l + ", Right = " + r + ", i = " + i);
+		if (l <= length-1 && array[l] > array[i])
+		{
+			largest = l;
+		}
+		else 
+		{
+			largest = i;
+		}
+		if (r <= length-1 && array[r] > array[largest])
+		{
+			largest = r;
+		}
+		System.out.println("largest = " + largest);
+		System.out.println("Current Array: " + Arrays.toString(array));
+		if (largest != i)
+		{
+			int temp = array [i];
+			array[i] = array[largest];
+			array[largest] = temp;
+			System.out.println("Hello");
+			maxHeapify(array,largest,length);
+		}
+		return array;
+	}
+	
+	public static int[] buildMaxHeap(int[] array)
+	{
+		for(int i = ((array.length)/2)-1; i >= 0; i--)
+		{
+			maxHeapify(array,i);
+		}
+		return array;
+	}
+	
+	public static int[] heapSort(int[] array)
+	{
+		buildMaxHeap(array);
+		System.out.println("Max Heap Array: " + Arrays.toString(array));
+		for (int i = array.length-1; i > 0; i-- )
+		{
+			int temp = array [0];
+			array[0] = array[i];
+			array[i] = temp;
+			maxHeapify(array,0,i);
+		}
+		return array;
+	}
+	
+	//QuickSort Methods - randomised
+	public static int[] quickSort(int[] array, int first, int last)
+	{
+		if (first < last)
+		{
+			int midpoint = partition(array, first, last);
+			quickSort(array, first, midpoint-1);
+			quickSort(array, midpoint+1, last);
+		}
+		return array;
+	}
+	
+	public static int partition(int[] array, int first, int last)
+	{
+		Random rand = new Random();
+		int i = (rand.nextInt(last - first)) + first;
+		int temp = array[last];
+		array[last] = array[i];
+		array[i] = temp;
+		int pointer = first - 1;
+		for (int j = first; j < last; j++)
+		{
+			if (array[j] <= array[last])
+			{
+				pointer++;
+				temp = array[pointer];
+				array[pointer] = array[j];
+				array[j] = temp;
+			}
+		}
+		temp = array[pointer + 1];
+		array[pointer + 1] = array[last];
+		array[last] = temp;
+		System.out.println("CURRENT ARRAY:  " + Arrays.toString(array));
+		return pointer +1;
+	}
 }
